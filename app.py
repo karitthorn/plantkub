5c050ea0720b481624e9a88fe1dc71839bd0b220
karitthorn <56643494+karitthorn@users.noreply.github.com>
2022-02-10 16:00:10 +0700

    fix bug

--- app.py
+++ app.py
@@ -8,16 +8,45 @@ app = Flask(__name__)
 @app.route("/",methods=["GET", "POST"]) #index (Thai)
 def index():
     ###############################################################
+    try:
+        url = 'http://blynk-cloud.com/cPs4vXujt7N9kL2SKXFJLiwCdf07oOuH/get/D17'
+        raw_page = req.get(url)
+        soup = BeautifulSoup(raw_page.text, 'html.parser')
+        soup = str(soup)
+        soup = soup.replace('[\"','')
+        soup = soup.replace('\"]','')
+        soup = int(soup)
+        soup = soup*100
+        soup = soup/800
+        soup = int(100-soup)
+        soup = str(soup)
+        percent = "%"
+        moisture = soup + percent
 
-    moisture = "UNKNOWN(ลองใหม่อีกครั้ง)"
-    light = "UNKNOWN(ลองใหม่อีกครั้ง)"
+        
+        url = 'http://blynk-cloud.com/cPs4vXujt7N9kL2SKXFJLiwCdf07oOuH/get/V1'
+        raw_page = req.get(url)
+        soup1 = BeautifulSoup(raw_page.text, 'html.parser')
+        soup1 = str(soup1)
+        if soup1 == "[\"1\"]":
+            light = "ON"
+        elif soup1 == "[\"0\"]":
+            light = "OFF"
+        else:
+            light = "UNKNOW"
+    except:
+        moisture = "UNKNOWN(ลองใหม่อีกครั้ง)"
+        light = "UNKNOWN(ลองใหม่อีกครั้ง)"
     ################################################################
     return render_template("index.html",moisture = moisture,light = light)
 
 @app.route("/openlight",methods=["GET", "POST"]) #index (Thai)
 def openlight():
-    url = 'http://blynk-cloud.com/cPs4vXujt7N9kL2SKXFJLiwCdf07oOuH/update/V1?value=1'
-    req.get(url)
+    try:
+        url = 'http://blynk-cloud.com/cPs4vXujt7N9kL2SKXFJLiwCdf07oOuH/update/V1?value=1'
+        req.get(url)
+    except:
+        openlight()
     ###############################################################
     try:
         url = 'http://blynk-cloud.com/cPs4vXujt7N9kL2SKXFJLiwCdf07oOuH/get/D17'
@@ -53,10 +82,12 @@ def openlight():
 
 @app.route("/turnofflight",methods=["GET", "POST"]) #index (Thai)
 def turnofflight():
-    url = 'http://blynk-cloud.com/cPs4vXujt7N9kL2SKXFJLiwCdf07oOuH/update/V1?value=0'
-    raw_page = req.get(url)
-
-###############################################################
+    try:
+        url = 'http://blynk-cloud.com/cPs4vXujt7N9kL2SKXFJLiwCdf07oOuH/update/V1?value=0'
+        raw_page = req.get(url)
+    except:
+        turnofflight()
+    ###############################################################
     try:
         url = 'http://blynk-cloud.com/cPs4vXujt7N9kL2SKXFJLiwCdf07oOuH/get/D17'
         raw_page = req.get(url)
@@ -101,8 +132,10 @@ def wateringtheplants():
 
 @app.route("/watertheplants",methods=["GET", "POST"]) #index (Thai)
 def watertheplants():
-
-    wateringtheplants()
+    try:
+        wateringtheplants()
+    except:
+        watertheplants()
     
     ###############################################################
     try:
