bfecf43e68426d16ce642d8044fa2ef925e9e0fc
karitthorn <56643494+karitthorn@users.noreply.github.com>
2022-02-10 15:34:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,35 +8,9 @@ app = Flask(__name__)
 @app.route("/",methods=["GET", "POST"]) #index (Thai)
 def index():
     ###############################################################
-    try:
-        url = 'http://blynk-cloud.com/cPs4vXujt7N9kL2SKXFJLiwCdf07oOuH/get/D17'
-        raw_page = req.get(url)
-        soup = BeautifulSoup(raw_page.text, 'html.parser')
-        soup = str(soup)
-        soup = soup.replace('[\"','')
-        soup = soup.replace('\"]','')
-        soup = int(soup)
-        soup = soup*100
-        soup = soup/800
-        soup = int(100-soup)
-        soup = str(soup)
-        percent = "%"
-        moisture = soup + percent
 
-        
-        url = 'http://blynk-cloud.com/cPs4vXujt7N9kL2SKXFJLiwCdf07oOuH/get/V1'
-        raw_page = req.get(url)
-        soup1 = BeautifulSoup(raw_page.text, 'html.parser')
-        soup1 = str(soup1)
-        if soup1 == "[\"1\"]":
-            light = "ON"
-        elif soup1 == "[\"0\"]":
-            light = "OFF"
-        else:
-            light = "UNKNOW"
-    except:
-        moisture = "UNKNOWN(ลองใหม่อีกครั้ง)"
-        light = "UNKNOWN(ลองใหม่อีกครั้ง)"
+    moisture = "UNKNOWN(ลองใหม่อีกครั้ง)"
+    light = "UNKNOWN(ลองใหม่อีกครั้ง)"
     ################################################################
     return render_template("index.html",moisture = moisture,light = light)
 
